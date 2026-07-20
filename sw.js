d94acb5076d42769a014821a1a919d0778a0d7fb
1Kazim <51178018+1Kazim@users.noreply.github.com>
2026-07-20 18:13:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "phase10-v2";
+const CACHE = "phase10-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
